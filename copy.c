8044965463847c651c443281fd07eed536b89611
Gethin Davies <gethin.davies@motilent.co.uk>
2023-08-09 13:17:23 +0100

    Add more error handling

--- copy.c
+++ copy.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <string.h>
 #include <unistd.h>
 
 static const int kWrongArgCount = 1;
@@ -9,9 +10,9 @@ static const int kSourceFileNonExistent = 2;
 static const int kDestFileOpenFailure = 4;
 static const int kReadFailure = 8;
 // Permissions: "rw-r--r--"
-static const int kDefeultPermissions = 0644;
+static const int kDefaultPermissions = 0644;
 
-// Simple copy file function without exhaustive error checking
+// Simple copy file function
 int main(int argc, char *argv[]) {
   if (argc != 3) {
     fprintf(stderr, "Usage:\n%s <source-file> <destination-file>\n", argv[0]);
@@ -19,11 +20,24 @@ int main(int argc, char *argv[]) {
   }
 
   int fdin = open(argv[1], O_RDONLY);
-  int fdout = open(argv[2], O_WRONLY | O_CREAT | O_EXCL, kDefeultPermissions);
+  if (fdin == -1) {
+    fprintf(stderr, "Error opening source file '%s':  %s\n", argv[1],
+            strerror(kSourceFileNonExistent));
+  }
+
+  int fdout = open(argv[2], O_WRONLY | O_CREAT | O_EXCL, kDefaultPermissions);
+  if (fdin == -1) {
+    fprintf(stderr, "Error opening destination file '%s':  %s\n", argv[2],
+            strerror(kDestFileOpenFailure));
+  }
 
   while (true) {
     char buffer[1024];
     ssize_t bytesRead = read(fdin, buffer, sizeof(buffer));
+    if (bytesRead == -1) {
+      fprintf(stderr, "Error reading source file '%s':  %s\n", argv[1],
+              strerror(kReadFailure));
+    }
     if (bytesRead == 0) {
       break;
     }
